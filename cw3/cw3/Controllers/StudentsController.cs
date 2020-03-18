8d97b47271b3f1697dba8c5233ea337058b0a8a2
lukonito <lukonito@gmail.com>
2020-03-18 11:09:55 +0100

    Add final

---
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using cw3.Models;
+using cw3.DAL;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudetn(int id)
+        {
+            
+            return Ok("Aktualizacja dokonczona");
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+
+            return Ok("Usuwanie ukonczone");
+        }
+
+        /*
+         [HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Adam, Jan, Kuba sortowanie={orderBy}";
+
+        } 
+         
+        [HttpGet]
+        //[HttpGet("getStudent")]
+        public string GetStudents()
+        {
+            return "Jan Kowalski";
+        }
+        
+        [HttpPost]
+        public string PostStudents([FromBody]Student student)
+        {
+            return "Jan Kowalski";
+        }
+          
+          
+        [HttpGet("{id}")]
+        public IActionResult GetStudents2([FromRoute] int id)
+        {
+            if (id == 1)
+                return Ok("Jan Kowalski");
+            if (id == 2)
+                return Ok("Adam Nowak");
+            return NotFound("Student not found");
+
+        }
+
+        [HttpGet("{id}")]
+        public string GetStudents2([FromRoute] int id)
+        {
+            if (id == 1)
+                return "Jan Kowalski ";
+            if (id == 2)
+                return "Adam Nowak";
+            return "Student not found";
+
+        }
+
+        [HttpPost]
+        public string PostStudents([FromQuery], [FromBody]Student student)
+        {
+            return "Jan Kowalski";
+        }
+        */
+    }
+}
---
+++ cw3/cw3/DAL/MockDbService.cs
@@ -0,0 +1,29 @@
+﻿using cw3.Models;
+using System.Collections.Generic;
+
+
+namespace cw3.DAL
+{
+    public class MockDbService : IDbService
+    {
+
+        private static IEnumerable<Student> _students;
+        
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent = 1, FirstName="Janek", LastName="Kowalczyk"},
+                new Student{IdStudent = 2, FirstName="Jola", LastName="Monola"},
+                new Student{IdStudent = 3, FirstName="Adam", LastName="Nowak"},
+
+            };
+        }
+
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
